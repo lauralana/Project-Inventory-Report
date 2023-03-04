f7c54670478032cb85709816f8a694d413e20fa0
Laura Lana <lauramarqueslana@gmail.com>
2023-03-03 22:07:02 -0300

    req 03

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,14 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, inventory):
+        response = super().generate(inventory)
+        company = Counter(row['nome_da_empresa'] for row in inventory)
+        in_stock = ''
+        for key, value in company.items():
+            in_stock += f'- {key}: {value}\n'
+        return (response + '\n' + 'Produtos estocados por empresa:\n{}'
+                ).format(in_stock)
