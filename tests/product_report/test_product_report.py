a79ad51fe31ed4768ebe5eb98310343718f3d150
Laura Lana <lauramarqueslana@gmail.com>
2023-03-04 18:37:56 -0300

    req 08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock = Product(
+        '3',
+        'NITROUS OXIDE',
+        'Galena Biopharma',
+        '2020-12-22',
+        '2024-11-07',
+        'CZ09 8588 0858 8435 9140 2695',
+        'instrucao 3'
+    )
+
+    assert str(mock) == (
+        'O produto NITROUS OXIDE fabricado em 2020-12-22 '
+        'por Galena Biopharma com validade até 2024-11-07 precisa '
+        'ser armazenado instrucao 3.'
+        )
