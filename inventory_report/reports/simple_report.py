cbcad3eed3a9fe8b68751890bc0fc41c27083bf0
Laura Lana <lauramarqueslana@gmail.com>
2023-03-03 21:30:08 -0300

    req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,18 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(inventory):
+        oldest_date = min([row['data_de_fabricacao'] for row in inventory])
+        company = Counter([row['nome_da_empresa'] for row in inventory])
+        expiration_date = min([row['data_de_validade'] for row in inventory
+                              if (row['data_de_validade'] > datetime.now()
+                               .strftime('%Y-%m-%d'))])
+
+        return(
+            'Data de fabricação mais antiga: {}\n'
+            'Data de validade mais próxima: {}\n'
+            'Empresa com mais produtos: {}'
+        ).format(oldest_date, expiration_date, company.most_common()[0][0])
